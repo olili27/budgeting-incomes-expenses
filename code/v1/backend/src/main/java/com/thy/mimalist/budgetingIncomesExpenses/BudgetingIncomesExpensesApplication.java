ec512aa0906d4ef7caf3497c9647b8f2daa11f4b
olili27 <timmolili27@gmail.com>
2026-08-25 20:43:29 +0300

    add backend bootstrap

---
+++ code/v1/backend/src/main/java/com/thy/mimalist/budgetingIncomesExpenses/BudgetingIncomesExpensesApplication.java
@@ -0,0 +1,13 @@
+package com.thy.mimalist.budgetingIncomesExpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetingIncomesExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetingIncomesExpensesApplication.class, args);
+	}
+
+}
---
+++ code/v1/backend/src/test/java/com/thy/mimalist/budgetingIncomesExpenses/BudgetingIncomesExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thy.mimalist.budgetingIncomesExpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetingIncomesExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
