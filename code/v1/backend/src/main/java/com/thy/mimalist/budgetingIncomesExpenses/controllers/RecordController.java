b6d1a2d9a88c092ab8d28543d19c14d92cf9b533
olili27 <timmolili27@gmail.com>
2026-09-14 22:54:35 +0300

    create records controller

--- code/v1/backend/src/main/java/com/thy/mimalist/budgetingIncomesExpenses/controllers/RecordController.java
+++ code/v1/backend/src/main/java/com/thy/mimalist/budgetingIncomesExpenses/controllers/RecordController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class RecordController {
 
     @GetMapping("/greet")
     public String returnGreeting() {
