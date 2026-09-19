07f886e4e2f17e2f302bdf243b4d30d64fb4ddc3
olili27 <timmolili27@gmail.com>
2026-09-19 23:33:55 +0300

    use @Repository annotation

--- code/v1/backend/src/main/java/com/thy/minimalist/budgetingIncomesExpenses/repository/CategoryRepository.java
+++ code/v1/backend/src/main/java/com/thy/minimalist/budgetingIncomesExpenses/repository/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.thy.minimalist.budgetingIncomesExpenses.repository;
 
 import com.thy.minimalist.budgetingIncomesExpenses.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 }
--- code/v1/backend/src/main/java/com/thy/minimalist/budgetingIncomesExpenses/repository/RecordLabelRepository.java
+++ code/v1/backend/src/main/java/com/thy/minimalist/budgetingIncomesExpenses/repository/RecordLabelRepository.java
@@ -2,6 +2,8 @@ package com.thy.minimalist.budgetingIncomesExpenses.repository;
 
 import com.thy.minimalist.budgetingIncomesExpenses.model.RecordLabel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecordLabelRepository extends JpaRepository<RecordLabel, Integer> {
 }
