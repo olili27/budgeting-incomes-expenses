706710b0259d76cac8ff1b6d81a4013283fb36e2
olili27 <timmolili27@gmail.com>
2026-09-21 23:46:40 +0300

    create categories controller

---
+++ code/v1/backend/src/main/java/com/thy/minimalist/budgetingIncomesExpenses/controller/CategoriesController.java
@@ -0,0 +1,25 @@
+package com.thy.minimalist.budgetingIncomesExpenses.controller;
+
+import com.thy.minimalist.budgetingIncomesExpenses.model.Category;
+import com.thy.minimalist.budgetingIncomesExpenses.service.interfaces.CategoryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/categories")
+public class CategoriesController {
+
+    private final CategoryService categoryService;
+
+    public CategoriesController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+}
